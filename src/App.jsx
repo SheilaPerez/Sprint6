d4de532d9301e631ca6c639bd95c1a82575642c4
SheilaPerez <sheilapg26@gmail.com>
2022-02-23 16:39:07 +0100

    Exercici1

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FraseEscena } from "./components/escena/Escena";
+
+export function App() {
+    return (
+         <FraseEscena /> 
+    )
+}
---
+++ src/components/escena/Escena.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function FraseEscena() {
+    return (
+        <p>El nostre heroï estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+    );
+}
