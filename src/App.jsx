51d0b5432b8625c5423a8d2ffdb58a3561d0f434
sheila <sheilapg26@gmail.com>
2022-03-02 15:55:33 +0100

    Exercici6

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import { FraseEscena } from "./components/escena/Escena";
 import { ButtonPrevious } from "./components/escena/ButtonPrevious"
 import { ButtonNext } from "./components/escena/ButtonNext"
 import { Welcome } from "./components/escena/Welcome"
+import primera from "./assets/img/primera.jpg"
+import segunda from "./assets/img/segunda.jpg"
+import tercera from "./assets/img/tercera.jpg"
+import cuarta from "./assets/img/cuarta.jpg"
+import { Background } from "./components/escena/Styled"
 
 export function App() {
     const [activeText, setActiveText] = useState(0);
     const [isLoggin, setIsLoggin] = useState(false);
 
-    const texts = [
-        "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-        "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-        "L'heroi va decidir travessar la porta que el portava a casa",
-        "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
-    ];
+    const texts = [{
+        text: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+        img: primera
+    }, {
+        text: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+        img: segunda
+    }, {
+        text: "L'heroi va decidir travessar la porta que el portava a casa",
+        img: tercera
+    }, {
+        text: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+        img: cuarta
+    }];
 
     const handleNextClickedNext = () => {
         if (activeText < texts.length-1) {
@@ -36,12 +48,14 @@ export function App() {
 
     return (
         <>
-            {isLoggin ? <>
-                            <ButtonPrevious previousClick={handleNextClickedPrevious}></ButtonPrevious>
-                            <ButtonNext nextClick={handleNextClickedNext}></ButtonNext>
-                            {texts.map((text, index) => <FraseEscena text={text} isActive={index === activeText} />)}
-                        </>
-                        : <Welcome welcomeBtn={handleClickWelcome}></Welcome>}
+
+            {isLoggin   ?   <Background activeBackground={texts[activeText].img}>
+                                <ButtonPrevious previousClick={handleNextClickedPrevious}></ButtonPrevious>
+                                <ButtonNext nextClick={handleNextClickedNext}></ButtonNext>
+                                {texts.map((text, index) => <FraseEscena text={text.text} isActive={index === activeText} />)}
+                            </Background>
+                        :   <Welcome welcomeBtn={handleClickWelcome}></Welcome>
+            }
         </>    
     )
 }
--- src/components/escena/Styled.js
+++ src/components/escena/Styled.js
@@ -6,13 +6,16 @@ export const BorderText = styled.p`
     text-align: center;
     border-radius: 20px;
     font-size: 15px;
-    background-color: ${props => props.isActive === true ? "#FFB2A6" : "white"};
+    background-color: ${props => props.isActive === true ? "#FFB2A6" : "#ffffff73"};
+    margin: 26px;
 `
 
 export const Buttons = styled.button`
     text-align: center;
-    padding: 10px 220px;
+    padding: 22px 205px;
     background-color: white;
+    font-size: 16px;
+    margin: 5px;
 `
 
 export const StartBtn = styled.button`
@@ -28,4 +31,10 @@ export const TextTutorial = styled.p`
     margin-top: 47px;
     text-align: center;
     color: #54BAB9;
+`
+
+export const Background = styled.div`
+    background-image: url(${props => props.activeBackground});
+    min-height: 100vh;
+    background-repeat: no-repeat;
 `
